788b40e102f81b3164ca9899523e11681711170b
gonzalezperezmario <73132372+gonzalezperezmario@users.noreply.github.com>
2020-11-16 20:08:16 +0100

    Add files via upload

---
+++ 1_Repositorio/README.md
@@ -0,0 +1,5 @@
+# Primer Repositorio
+Autor: Mario González
+Ejercicio 1
+
+Ejercicio básico sobre insertar datos a una colección
---
+++ 1_Repositorio/src/1_Repositorio.js
@@ -0,0 +1,7 @@
+/*En nuestro primer proyecto vaos ha insertar un documento en la colección 1 de la base de datos*/ 
+
+db . col001 . insertOne ({
+    "nombre" : "Antonio" ,
+    "apellidos" : "Rivas del Rey" ,
+    "edad" : 33
+   })
